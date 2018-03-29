11a6f801867435cfbc9bf4bfc7ad9f0454aeecc1
Hieu Tran <hieutran.th.358@gmail.com>
2018-03-29 10:05:49 +0300

    Add topic 3

---
+++ Lesssons/3.Time Complexity/PermMissingElem.cpp
@@ -0,0 +1,52 @@
+/*
+
+
+A zero-indexed array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+Your goal is to find that missing element.
+
+Write a function:
+
+    int solution(vector<int> &A);
+
+that, given a zero-indexed array A, returns the value of the missing element.
+
+For example, given array A such that:
+  A[0] = 2
+  A[1] = 3
+  A[2] = 1
+  A[3] = 5
+
+the function should return 4, as it is the missing element.
+
+Assume that:
+
+        N is an integer within the range [0..100,000];
+        the elements of A are all distinct;
+        each element of array A is an integer within the range [1..(N + 1)].
+
+Complexity:
+
+        expected worst-case time complexity is O(N);
+        expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
+
+*/
+
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    vector<int> B;
+    B.resize(A.size()+1);
+    for(int i = 0; i < A.size(); i++){
+        int x = A[i];
+        B[x-1] = x;
+    }
+    for(int i = 0; i < B.size(); i++){
+        if (B[i] == 0) return i+1;   
+    }
+}

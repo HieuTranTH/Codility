4eb8a05a14a175ed9c3524bdc8b999da24e3dc1e
Hieu Tran <hieutran.th.358@gmail.com>
2018-04-17 12:16:59 +0300

    Add 4.PermCheck

---
+++ Lessons/4.Counting Elements/PermCheck.cpp
@@ -0,0 +1,75 @@
+
+
+A non-empty zero-indexed array A consisting of N integers is given.
+
+A permutation is a sequence containing each element from 1 to N once, and only once.
+
+For example, array A such that:
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+
+is a permutation, but array A such that:
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+
+is not a permutation, because value 2 is missing.
+
+The goal is to check whether array A is a permutation.
+
+Write a function:
+
+    int solution(vector<int> &A);
+
+that, given a zero-indexed array A, returns 1 if array A is a permutation and 0 if it is not.
+
+For example, given array A such that:
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+
+the function should return 1.
+
+Given array A such that:
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+
+the function should return 0.
+
+Assume that:
+
+        N is an integer within the range [1..100,000];
+        each element of array A is an integer within the range [1..1,000,000,000].
+
+Complexity:
+
+        expected worst-case time complexity is O(N);
+        expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+
+    bool checkArray[A.size()];
+    for (int i = 0; i < A.size(); i++){
+        checkArray[i] = false;   
+    }
+    
+    for (int i = 0; i < A.size(); i++){
+        if ((A[i] <= 0) || (A[i] > A.size())) return false;
+        
+        if (checkArray[A[i] - 1]) return false;
+        else checkArray[A[i] - 1] = true;
+        
+    }
+    return true;
+}
